e5c324c78ea5cfb32c25150ff2e3bdf7571584f4
Marvyn Mark <marvyn.mark@gmail.com>
2020-12-15 21:01:54 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -49,5 +49,16 @@ namespace SalesWebMvc.Controllers {
             _SellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id) {
+            if (id == null) {
+                return NotFound();
+            }
+            var obj = _SellerService.FindById(id.Value);
+            if (obj == null) {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
